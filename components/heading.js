54fc5c3fe9d1fa15ea8f07d30145a049d85d4428
satishsuresh <69952130+satishsuresh@users.noreply.github.com>
2020-09-24 14:40:57 +0530

    installing bootstrap

---
+++ components/heading.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export class Heading extends Component {
+  render() {
+    return (
+      <div>
+        Notes <button className="btn btn-success">excited</button>
+      </div>
+    );
+  }
+}
