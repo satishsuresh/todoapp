110121103a0618a380a6cb0ae37b014ecb53b84d
satishsuresh <69952130+satishsuresh@users.noreply.github.com>
2020-09-24 14:01:54 +0530

    installing bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
-
+import { heading } from "../component/heading";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <div>project begins</div>
+      <heading />
     </div>
   );
 }
